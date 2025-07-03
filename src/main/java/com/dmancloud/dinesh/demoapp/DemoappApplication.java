c352b57f7741f6c3240a92f32588857de00f932b
Danish <you@example.com>
2025-07-03 15:28:36 +0000

    update

--- src/main/java/com/dmancloud/dinesh/demoapp/DemoappApplication.java
+++ src/main/java/com/dmancloud/dinesh/demoapp/DemoappApplication.java
@@ -16,4 +16,7 @@ public class DemoappApplication {
         String password = "admin123"; // hardcoded password
         System.out.println("Using hardcoded password: " + password);
     }
+     public static void logPassword(String password) {
+        LOGGER.log(Level.INFO, "User password is: {0}", password); // Logging sensitive data
+    }
 }
